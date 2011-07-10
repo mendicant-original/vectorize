7d66ef76b0c01876292e470d3832e20226581755
Andy Lindeman <alindeman@gmail.com>
2011-07-10 16:29:48 -0400

    Take a first stab at the DSL

---
+++ lib/vectorize/dsl.rb
@@ -0,0 +1,38 @@
+module Vectorize
+  # Example usage:
+  # Vectorize.draw(200, 200) do |v|
+  #   v.move_to(100, 100)
+  #   v.line_to(175, 175)
+  #   v.stroke
+  #
+  #   v.save_as "foo.png"
+  # end
+  def self.draw(width, height)
+    surface = Surfaces::Image.new(width, height)
+    dsl     = DSL.new(surface)
+
+    yield dsl
+  end
+
+  class DSL
+    def initialize(surface)
+      @surface = surface
+    end
+
+    def move_to(x, y)
+      CairoWrapper.cairo_move_to(@surface.context, x, y)
+    end
+
+    def line_to(x, y)
+      CairoWrapper.cairo_line_to(@surface.context, x, y)
+    end
+
+    def stroke
+      CairoWrapper.cairo_stroke(@surface.context)
+    end
+
+    def save_as(filename)
+      @surface.write_to_png(filename)
+    end
+  end
+end
